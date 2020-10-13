8500b41308b2be67d8e299057fd094b3c9d561dd
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2020-10-13 21:39:54 +0300

    Support handling configured but unavailable corpora
    Support handling corpora defined in the configuration but unavailable to
    the backend, depending on settings.handleUnavailableCorpora: either they
    can be fatal as before ("fatal", "none" or "undefined"), or they can be
    filtered out of the configuration, with a message on the JavaScript
    console ("error", "warn" or "log").
    Filtering out unavailable corpora requires that the backend /corpus_info
    understands the parameter report_undefined_corpora.

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -284,16 +284,25 @@ korpApp.factory("searches", [
 
             getInfoData() {
                 const def = $q.defer()
+                const handle_unavailable_corpora =
+                      ! (["none", "fatal", undefined].includes(
+                          settings.handleUnavailableCorpora))
                 const conf = util.httpConfAddMethod({
                     url: settings.korpBackendURL + "/corpus_info",
                     params: {
                         corpus: _.map(settings.corpusListing.corpora, "id")
                             .map((a) => a.toUpperCase())
                             .join(","),
+                        report_undefined_corpora: (handle_unavailable_corpora
+                                                   ? "true" : "false"),
                     },
                 })
                 $http(conf).then(function (response) {
                     const { data } = response
+                    if (handle_unavailable_corpora) {
+                        util.removeUnavailableCorpora(
+                            settings.corpusListing.corpora, data)
+                    }
                     for (let corpus of settings.corpusListing.corpora) {
                         corpus["info"] = data["corpora"][corpus.id.toUpperCase()]["info"]
                         const privateStructAttrs = []
