92643cc7dffa030eefb777a0498cbec05e5b1578
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2021-09-28 17:32:18 +0300

    sidebar_link_section: Convert to a plugin
    app/plugins/sidebar_link_section/scripts/main.js:
    - Convert to a plugin class with a callback for hook point
      modifyCorpusConfigs.
    - Convert to using camelCase.
    - Only the corpus configurations are currently modified: no link section
      is added to the sidebar yet.

--- app/plugins/sidebar_link_section/scripts/main.js
+++ app/plugins/sidebar_link_section/scripts/main.js
@@ -1,55 +1,74 @@
 
-// Initialize the link_attributes properties in all the corpora in
-// settings.corpora.
-util.initCorpusSettingsLinkAttrs = function() {
-    for (let corpus in settings.corpora) {
-        util.extractLinkAttrs(settings.corpora[corpus]);
+// Plugin sidebar_link_section
+//
+// Callback methods to initialize linkAttributes property in corpus
+// configurations and to render it in the sidebar
+
+
+// Plugin class
+
+class MakeSidebarLinkSection {
+
+    // Callback methods
+
+    // Initialize the linkAttributes properties in all corpora
+    modifyCorpusConfigs (corpora, corporafolders) {
+        this._initCorpusSettingsLinkAttrs(corpora)
     }
-    return null;
-};
 
-// Initialize the link_attributes property in corpusInfo to contain
-// the attributes with type "url" and url_opts.in_link_section true.
-// These attributes are shown in a separate section in the sidebar.
-// The original attributes are marked as hidden.
-util.extractLinkAttrs = function(corpusInfo) {
+    // Internal methods
 
-    const extractLinkAttrs = function(attrs, link_attrs) {
-        if (attrs != null) {
-            return (() => {
-                const result = [];
+    // Initialize the linkAttributes properties in all the corpora in
+    // settings.corpora.
+    _initCorpusSettingsLinkAttrs (corpora) {
+        for (let corpus in corpora) {
+            this._extractLinkAttrs(corpora[corpus])
+        }
+    };
+
+    // Initialize the linkAttributes property in corpusInfo to contain
+    // the attributes with type "url" and urlOpts.inLinkSection true.
+    // These attributes are shown in a separate section in the sidebar.
+    // The original attributes are marked as hidden.
+    _extractLinkAttrs (corpusInfo) {
+
+        const extractLinkAttrs = function (attrs, linkAttrs) {
+            if (attrs) {
                 for (let attrname in attrs) {
-                    const attr = attrs[attrname];
-                    if ((attr.type === "url") && (attr.url_opts != null) && attr.url_opts.in_link_section) {
-                        if (attr._link_attr) {
+                    const attr = attrs[attrname]
+                    if ((attr.type === "url") &&
+                            (attr.urlOpts != null) &&
+                            attr.urlOpts.inLinkSection) {
+                        if (attr._linkAttr) {
                             // This attribute was already handled via
                             // another reference; that happens when many
                             // corpora use the same attribute definitions
                             // objects.
-                            result.push(link_attrs[attrname] = attr._link_attr);
+                            linkAttrs[attrname] = attr._linkAttr
                         } else {
                             // Make a deep copy of the attr object
-                            link_attrs[attrname] = $.extend(true, {}, attr);
+                            linkAttrs[attrname] = $.extend(true, {}, attr)
                             // The original attribute cannot be deleted
                             // (without making more modifications
                             // elsewhere) because Korp only requests from
                             // the backend the attributes mentioned in
-                            // attributes or struct_attributes.
-                            attrs[attrname].displayType = "hidden";
-                            result.push(attrs[attrname]._link_attr = link_attrs[attrname]);
+                            // attributes or structAttributes.
+                            attrs[attrname].displayType = "hidden"
+                            attrs[attrname]._linkAttr = linkAttrs[attrname]
                         }
-                    } else {
-                        result.push(undefined);
                     }
                 }
-                return result;
-            })();
+            }
         }
-    };
 
-    const link_attrs = {};
-    extractLinkAttrs(corpusInfo.attributes, link_attrs);
-    extractLinkAttrs(corpusInfo.struct_attributes, link_attrs);
-    corpusInfo.link_attributes = link_attrs;
-    return null;
-};
+        const linkAttrs = {}
+        extractLinkAttrs(corpusInfo.attributes, linkAttrs)
+        extractLinkAttrs(corpusInfo.structAttributes, linkAttrs)
+        corpusInfo.linkAttributes = linkAttrs
+    }
+
+}
+
+
+// Register the plugin
+plugins.register(new MakeSidebarLinkSection)
