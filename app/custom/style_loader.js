52f45eb17b5a3c6f833d02f4c4879945640deebc
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2021-09-28 14:51:28 +0300

    Sidebar: Hanging indent; tighter intra-paragraph spacing
    Add app/styles/sidebar_mods.scss:
    - Sidebar style modifications:
      - Hanging indent for attributes and corpus information items.
      - Slightly tighter line-spacing in multi-line values than between
        items.
    
    app/custom/style_loader.js:
    - Load sidebar_mods.scss.

--- app/custom/style_loader.js
+++ app/custom/style_loader.js
@@ -6,3 +6,6 @@ function requireAll(r) { r.keys().forEach(r) }
 
 // Load all styles under ../styles/kielipankki
 requireAll(require.context("../styles/kielipankki", true, /\.s?css$/))
+
+// Sidebar style modifications
+require("../styles/sidebar_mods.scss")
