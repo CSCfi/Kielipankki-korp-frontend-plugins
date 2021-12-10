1ee643e33a069aa0a800a72e20adb6d9e4ecb87c
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2021-12-10 14:02:07 +0200

    webpack.common.js: Copy markup files from config dir

--- webpack.common.js
+++ webpack.common.js
@@ -194,6 +194,11 @@ module.exports = {
                     noErrorOnMissing: true,
                 },
                 {
+                    from: korpConfigDir + "/markup/*html",
+                    to: "markup/[name].[ext]",
+                    noErrorOnMissing: true,
+                },
+                {
                     from: "app/translations/angular-locale_*.js",
                     to: "translations/[name].[ext]",
                 },
