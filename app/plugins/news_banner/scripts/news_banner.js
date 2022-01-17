32991b991c1f2b3e6386296b8be64c41497c9f4d
Jyrki Niemi <jyrki.niemi@helsinki.fi>
2022-01-17 10:17:35 +0200

    news_banner: Convert to a plugin
    app/plugins/news_banner/scripts/news_banner.js:
    - Modify heavily the Korp newsdesk code to tailor it for the closeable
      news banner.
    
    Add app/plugins/news_banner/styles/news_banner.scss:
    - News banner styles.
    
    Add app/plugins/news_banner/includes/top_header.pug:
    - Add the news banner to the top of the Korp window (top_header).

--- app/plugins/news_banner/scripts/news_banner.js
+++ app/plugins/news_banner/scripts/news_banner.js
@@ -1,127 +1,131 @@
-/** @format */
-// SB-newsdesk 1.0b
-// Requirements: JQuery, JQuery.ui.position, trust filter, loc filter, Font Awesome
 
-angular.module("newsdesk", []).directive("newsDesk", ($window, $document, $rootElement, $http, $location) => ({
-    template: `
-            <div>
-                <div ng-if="shouldUseThis" class="newsdesk-opener" 
-                     ng-click="togglePopover($event)" 
-                     ng-class="{'newsdesk-new-news': numNewNews != 0, 'newsdesk-no-new-news' : numNewNews == 0}">
-                    <i class="fa fa-bell newsdesk-bell"></i>
-                    <div class="newsdesk-arrow-box">
-                        <span>{{numNewNews}}</span>
-                    </div>&nbsp;
-                </div>
-                <div class="popover newsdesk-popover" ng-click="onPopoverClick($event)" to-body>
-                    <div class="arrow"></div>
-                    <h2 class="popover-title">{{header | loc:lang}}<span style="float:right;cursor:pointer" ng-click="popHide()">×</span></h2>
-                    <div class="newsdesk-around-items">
-                        <div class="newsdesk-news-item" ng-repeat="item in newsitems" 
-                             ng-class="{'newsdesk-new-news-item': (item.d > lastChecked)}">
-                            <h4>{{item.t[currentLang]}}</h4>
-                            <span class="newsdesk-item-date">{{item.d}}</span>
-                            <div ng-bind-html="item.h[currentLang] | trust"></div>
+// Plugin news_banner
+//
+// An AngularJS module implementing a closeable news banner for
+// important news, typically at the very top of the Korp window, read
+// from a JSONP source by default specified by settings.newsBannerUrl.
+// Once the banner is closed, it is not shown before new news are
+// added and the page is reloaded.
+//
+// Based on Korp's newsdesk.js (SB-newsdesk 1.0b).
+// Requirements: trust filter
+
+
+// Register AngularJS module as a dependency to the Korp app
+plugins.registerAngularModule("newsbanner", []).directive(
+    "newsBanner",
+    ($window, $location) => ({
+        template: `
+                <div id="news_banner" ng-if="showBanner">
+                    <div id="news_banner_content">
+                        <div class="newsbanner-item" ng-repeat="item in newsItems">
+                            <div class="newsbanner-item-content"
+                                 ng-bind-html="item.h[currentLang] | trust"></div>
                         </div>
                     </div>
+                    <div id="news_banner_close" ng-click="closeBanner()">×</div>
                 </div>
-        </div>`,
-    restrict: "EA",
-    replace: true,
-    scope: { header: "=", storage: "=" },
-    link(scope, elem, attr) {
-        const s = scope
-        s.shouldUseThis = settings.newsDeskUrl != null
+            `,
+        restrict: "EA",
+        scope: {
+            urlsetting: "=",
+            storage: "=",
+            callback: "=",
+            newsItems: "<",
+        },
+        link(scope, elem, attr) {
+            const s = scope
 
-        if (!s.shouldUseThis) {
-            return
-        }
+            // Use default values if not specified as attributes
+            s.urlsetting = s.urlsetting || "newsBannerUrl"
+            s.storage = s.storage || "korp_newsbanner_last_closed"
+            // callback value would appear to have to be different
+            // from the value for the newsdesk
+            s.callback = s.callback || "newsbannerdata"
 
-        s.onPopoverClick = (event) => event.stopPropagation()
+            const newsBannerUrl = $window.settings[s.urlsetting]
+            s.showBanner = (newsBannerUrl != null)
 
-        s.newsitems = []
-        function initData() {
-            let d
-            s.lastChecked = localStorage.getItem(s.storage)
-            if (!s.lastChecked) {
+            if (! s.showBanner) {
+                return
+            }
+            s.newsItems = []
+
+            // Initialize data from the URL newsBannerUrl
+            function initData() {
+                let d
+                s.lastClosed = localStorage.getItem(s.storage)
+                // Initially, set lastClosed to one year back
+                if (! s.lastClosed) {
+                    d = new Date()
+                    d.setFullYear(d.getFullYear() - 1)
+                    s.lastClosed = d.toISOString().slice(0, 10)
+                }
+                // c.log("banner last closed", s.storage, s.lastClosed)
+                // Get the news data
+                $.ajax({
+                    method: "GET",
+                    url: newsBannerUrl,
+                    async: false,
+                    jsonpCallback: s.callback,
+                    contentType: "application/javascript",
+                    dataType: "jsonp",
+                    success(json) {
+                        const currentDate = new Date().toISOString().slice(0, 10)
+                        s.newsItems = _.filter(json, (newsItem) => {
+                            // Include news that are from today or
+                            // earlier but after the date when the
+                            // banner was last closed and that either
+                            // have no expiration date or the
+                            // expiration date is today or later.
+                            // TODO: Allow configuring if future news
+                            // should be shown or not
+                            return (newsItem.d <= currentDate &&
+                                    newsItem.d > s.lastClosed &&
+                                    (! newsItem.e || newsItem.e >= currentDate))
+                        })
+                        s.newsItems = _.map(s.newsItems, (newsItem) => {
+                            // Incorporate the date and title to the body
+                            // of the news item
+                            for (const lang in newsItem.h) {
+                                newsItem.h[lang] = `
+                                    <p class="newsbanner-item-heading">
+                                      <span class="newsbanner-item-date">${newsItem.d}</span>
+                                      <span class="newsbanner-item-title">${newsItem.t[lang]}</span>
+                                    </p>
+                                    ${newsItem.h[lang]}`
+                            }
+                            return newsItem
+                        })
+                        // Show banner if there are news to show
+                        s.showBanner = s.newsItems.length > 0
+                    },
+                    error(e) {
+                        console.log("error, couldn't fetch banner news", e)
+                    },
+                })
+                s.showBanner = s.newsItems.length > 0
+            }
+
+            // Close the banner and save today as the the last closed
+            // date to localStorage
+            s.closeBanner = function () {
+                s.showBanner = false
                 d = new Date()
-                d.setFullYear(d.getFullYear() - 1)
-                s.lastChecked = d.toISOString().slice(0, 10)
+                s.lastClosed = d.toISOString().slice(0, 10)
+                localStorage.setItem(s.storage, s.lastClosed)
             }
-            $.ajax({
-                type: "GET",
-                url: settings.newsDeskUrl,
-                async: false,
-                jsonpCallback: "newsdata",
-                contentType: "application/json",
-                dataType: "jsonp",
-                success(json) {
-                    const currentDate = new Date().toISOString().slice(0, 10)
-                    s.newsitems = _.filter(json, (newsitem) => {
-                        return !newsitem.e || newsitem.e >= currentDate
-                    })
-                    let n = 0
-                    for (let nItem of s.newsitems) {
-                        if (nItem.d > s.lastChecked) {
-                            n += 1
-                        }
-                    }
 
-                    safeApply(s, () => (s.numNewNews = n))
-                },
+            s.currentLang = $location.search().lang || settings.defaultLanguage
 
-                error(e) {
-                    console.log("error, couldn't fetch news", e.message)
-                },
-            })
-        }
+            // Change language whenever the UI language is changed
+            s.$watch(
+                () => $location.search().lang,
+                (lang) => (s.currentLang = lang || settings.defaultLanguage)
+            )
 
-        s.currentLang = $location.search().lang || settings.defaultLanguage
+            initData()
 
-        s.numNewNews = 0
-        initData()
-
-        s.togglePopover = function (event) {
-            if (s.isPopoverVisible) {
-                s.popHide()
-            } else {
-                s.currentLang = $location.search().lang || settings.defaultLanguage
-                s.popShow()
-                s.numNewNews = 0
-            }
-            event.preventDefault()
-            event.stopPropagation()
-        }
-
-        const popover = $(".newsdesk-popover")
-        s.isPopoverVisible = false
-
-        const handleEscape = function (event) {
-            if (event.which === 27) {
-                s.popHide()
-                return false
-            }
-        }
-
-        s.popShow = function () {
-            s.isPopoverVisible = true
-
-            popover.show().focus().position({
-                my: "right top",
-                at: "right-10 top+10",
-                of: window,
-            })
-            $rootElement.on("keydown", handleEscape)
-            $rootElement.on("click", s.popHide)
-
-            localStorage.setItem(s.storage, s.newsitems[0].d)
-        }
-
-        s.popHide = function () {
-            s.isPopoverVisible = false
-            popover.hide()
-            $rootElement.off("keydown", handleEscape)
-            $rootElement.off("click", s.popHide)
-        }
-    },
-}))
+        },
+    })
+)
